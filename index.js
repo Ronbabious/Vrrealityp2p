fbf088c7481c90744953a1ea05ba62657de17640
Ronbabious <au577483@uni.au.dk>
2020-09-14 15:40:35 +0200

    pie test3

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Gpio = require('onoff').Gpio // #A
 const sensorLib = require('node-dht-sensor')
+const Sonar = require('raspi-hc-sr04');
+
 
 sensorLib.initialize(11, 12) // #A
 const led = new Gpio(4, 'out') // #B
+const sonar = new Sonar({triggerPin: 23, echoPin: 24});
 
 const interval = setInterval(() => { // #C
  let value = (led.readSync() + 1) % 2 // #D
@@ -11,6 +14,11 @@ const interval = setInterval(() => { // #C
  led.write(value, () => { // #E
  })
 }, 2000)
+
+sonar.read((duration => {
+    let distance = 343.0 * duration / 10000* .5
+    console.log(`duration: ${ duration } distance: ${ distance} cm`)
+}))
 process.on('SIGINT', () => { // #F
  clearInterval(interval)
  led.writeSync(0) // #G
