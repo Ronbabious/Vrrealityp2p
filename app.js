a29075a923293693ad2b32e78497a3cb7aa5a79a
Ronbabious <au577483@uni.au.dk>
2020-09-15 14:45:18 +0200

    testing implementation

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const path = '/'
+const path = process.cwd(); // Get current path for process
 const port = 8080;
+console.log(path);
 
-router.use(function (req,res,next) {
-    console.log('/' + req.method);
-    next();
-  });
+router.use(function(req, res, next) {
+	console.log('/' + req.method);
+	next();
+});
 
 router.get('/', function(req, res) {
-    res.sendFile('index.js')
-})
+	res.sendFile(path + 'blank.js');
+});
 
 app.use(express.static(path));
 app.use('/', router);
 
-app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.listen(port, function() {
+	console.log('Example app listening on port 8080!');
+});
--- index.js
+++ index.js
@@ -1,45 +1,56 @@
-const gpio = require('onoff').Gpio // #A
-const sensorLib = require('node-dht-sensor')
+const gpio = require('onoff').Gpio; // #A
+const sensorLib = require('node-dht-sensor');
 const Gpio = require('pigpio').Gpio;
 
-const MICROSECDONDS_PER_CM = 1e6/34321;
-const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
-const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+const MICROSECDONDS_PER_CM = 1e6 / 34321;
+const trigger = new Gpio(23, { mode: Gpio.OUTPUT });
+const echo = new Gpio(24, { mode: Gpio.INPUT, alert: true });
+
+var temp = document.getElementById('temp');
+var hum = document.getElementById('hum');
+var dis = document.getElementById('dis');
 
 trigger.digitalWrite(0); // Make sure trigger is low
-sensorLib.initialize(11, 12) // #A
-const led = new gpio(4, 'out') // #B
+sensorLib.initialize(11, 12); // #A
+const led = new gpio(4, 'out'); // #B
 
 const watchHCSR04 = () => {
-    let startTick;
-  
-    echo.on('alert', (level, tick) => {
-      if (level == 1) {
-        startTick = tick;
-      } else {
-        const endTick = tick;
-        const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-        const sonic = diff / 2 / MICROSECDONDS_PER_CM;
-        console.log('Distance in cm: ' + sonic + '.');
-      }
-    });
-  };
-  
+	let startTick;
+
+	echo.on('alert', (level, tick) => {
+		if (level == 1) {
+			startTick = tick;
+		} else {
+			const endTick = tick;
+			const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+			sonic = diff / 2 / MICROSECDONDS_PER_CM;
+			console.log('Distance in cm: ' + sonic + '.');
+		}
+	});
+};
 
 watchHCSR04();
-const interval = setInterval(() => { // #C
- let value = (led.readSync() + 1) % 2 // #D
- let readout = sensorLib.read();
- trigger.trigger(10, 1); // Set trigger high for 10 microseconds
- console.log('Temperature izt: ' + readout.temperature.toFixed(2) + 'C, ' + 'Humidity: ' + readout.humidity.toFixed(2) + '%')
- led.write(value, () => { // #E
- })
-}, 2000)
+const interval = setInterval(() => {
+	// #C
+	let value = (led.readSync() + 1) % 2; // #D
+	let readout = sensorLib.read();
+	trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+	dis.append(sonic);
+	temp.append(readout.temperature.toFixed(2));
+	hum.append(readout.humidity.toFixed(2));
+	console.log(
+		'Temperature izt: ' + readout.temperature.toFixed(2) + 'C, ' + 'Humidity: ' + readout.humidity.toFixed(2) + '%'
+	);
+	led.write(value, () => {
+		// #E
+	});
+}, 2000);
 
-process.on('SIGINT', () => { // #F
- clearInterval(interval)
- led.writeSync(0) // #G
- led.unexport()
- console.log('Bye, bye!')
- process.exit()
-})
+process.on('SIGINT', () => {
+	// #F
+	clearInterval(interval);
+	led.writeSync(0); // #G
+	led.unexport();
+	console.log('Bye, bye!');
+	process.exit();
+});
