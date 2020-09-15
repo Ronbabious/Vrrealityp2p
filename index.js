054568ffe7e26839a27e6dadb07bcb69a63e5435
Ronbabious <au577483@uni.au.dk>
2020-09-15 10:01:55 +0200

    pie test3

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const Gpio = require('onoff').Gpio // #A
 const sensorLib = require('node-dht-sensor')
-const Sonar = require('raspi-hc-sr04');
+const Gpio = require('pigpio').Gpio;
 
+const MICROSECDONDS_PER_CM = 1e6/34321;
+const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
+const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
 
+trigger.digitalWrite(0); // Make sure trigger is low
 sensorLib.initialize(11, 12) // #A
 const led = new Gpio(4, 'out') // #B
-const sonar = new Sonar({triggerPin: 23, echoPin: 24});
+
+const watchHCSR04 = () => {
+    let startTick;
+  
+    echo.on('alert', (level, tick) => {
+      if (level == 1) {
+        startTick = tick;
+      } else {
+        const endTick = tick;
+        const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+        console.log(diff / 2 / MICROSECDONDS_PER_CM);
+      }
+    });
+  };
+  
+watchHCSR04();
 
 const interval = setInterval(() => { // #C
  let value = (led.readSync() + 1) % 2 // #D
  let readout = sensorLib.read();
+ trigger.trigger(10, 1); // Set trigger high for 10 microseconds
  console.log('Temperature izt: ' + readout.temperature.toFixed(2) + 'C, ' + 'Humidity: ' + readout.humidity.toFixed(2) + '%')
  led.write(value, () => { // #E
  })
 }, 2000)
 
-sonar.read((duration => {
-    let distance = 343.0 * duration / 10000* .5
-    console.log(`duration: ${ duration } distance: ${ distance} cm`)
-}))
 process.on('SIGINT', () => { // #F
  clearInterval(interval)
  led.writeSync(0) // #G
