b894d24ef37195516426e73b81f1b3a88bb87474
Pie <asd>
2020-09-21 13:40:05 +0200

    can turn on both leds

--- wot-server.js
+++ wot-server.js
@@ -8,3 +8,9 @@ var httpServer = require('./http'),
 var server = httpServer.listen(resources.pi.port, function () {
     console.info('Your WoT Pi is up and running on port %s', resources.pi.port);
 });
+
+
+//curl -i -H "Content-Type: application/json" \
+//-H "Accept: application/json" \
+//-X PUT 'http://192.168.43.15:3000/pi/actuators/leds/1' \
+//-d '{"value":true}'
